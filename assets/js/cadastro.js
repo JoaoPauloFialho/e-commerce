00bfac34194bab5b1a28d95e74adb55cfff0a8ca
JoaoPauloFialho <jpcarneirofialho@gmail.com>
2022-11-20 18:27:32 -0300

    Adicionei mais validações de cadastro, como a checagem se um usuário está ou não logado, e usando localstorage do browser adicionei uma validação no login para saber se o usuário está ou não cadastrado. Além de modificar o .gitignore

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,97 +1,111 @@
 //seletores da página cadastro
-const formulario = document.querySelector('.formulario_login_cadastro')
-const usuario = document.querySelector('.usuario_login_cadastro')
-const senha = document.querySelector('.senha')
-const repetir_senha = document.querySelector('.repetir_senha')
-const botao = document.querySelector('.button_login_cadastro')
-const mostrar_senha = document.querySelector('.img_mostrar_senha')
+const formulario = document.querySelector('.formulario_login_cadastro');
+const usuario = document.querySelector('.usuario_login_cadastro');
+const senha = document.querySelector('.senha');
+const repetir_senha = document.querySelector('.repetir_senha');
+const botao = document.querySelector('.button_login_cadastro');
+const mostrar_senha = document.querySelector('.img_mostrar_senha');
 
 //lista para salvar no local storage, se ela não existir eu instancio uma
-const usuarios_cadastrados = JSON.parse(localStorage.getItem("usuarios_cadastrados")) || []
+const usuarios_cadastrados = JSON.parse(localStorage.getItem("usuarios_cadastrados")) || [];
 
 //adiciona um evento de submit
-formulario.addEventListener('submit', e =>{
+formulario.addEventListener('submit', e => {
     handleSubmit(e);
 })
 
 //controla o evento de submit fazendo a checagem do formulário
-function handleSubmit(e){
-    remove_erros()
-    e.preventDefault()
-    const campos_validos = valida_campos()
-    const senha_valida = valida_senha()
+function handleSubmit(e) {
+    remove_erros();
+    e.preventDefault();
+    const campos_validos = valida_campos();
+    const senha_valida = valida_senha();
+    const usuario_valido = valida_usuario();
 
-    if(campos_validos && senha_valida){
-        remove_erros()
-        adiciona_armazenamento_local(usuario.value, senha.value)
-        alert('Usuário Cadastrado')
+    if (campos_validos && senha_valida && usuario_valido) {
+        remove_erros();
+        adiciona_armazenamento_local(usuario.value, senha.value);
+        alert('Usuário Cadastrado');
 
-        formulario.submit()
-        window.location.assign("login.html")
+        formulario.submit();
+        window.location.assign("login.html");
     }
 }
 
 //remove todas as mensagens de erro
-function remove_erros(){
-    const erros = formulario.querySelectorAll('.mensagem_erro')
-    Array.from(erros).forEach(e=>{
-        e.remove()
+function remove_erros() {
+    const erros = formulario.querySelectorAll('.mensagem_erro');
+    Array.from(erros).forEach(e => {
+        e.remove();
     })
 }
 
 //cria uma mensagem de erro
-function cria_erro(campo, msg){
-    const erro_p = document.createElement('p')
-    erro_p.innerHTML = msg
-    erro_p.classList.add('mensagem_erro')
-    campo.insertAdjacentElement('afterend', erro_p) 
+function cria_erro(campo, msg) {
+    const erro_p = document.createElement('p');
+    erro_p.innerHTML = msg;
+    erro_p.classList.add('mensagem_erro');
+    campo.insertAdjacentElement('afterend', erro_p);
 }
 
 //função para validar a senha
-function valida_senha(){
+function valida_senha() {
     let valido = true;
-    if(senha.value.length < 6){
-        valido = false;
+    if (senha.value.length < 6) {
         cria_erro(senha, 'Senha muito curta');
+        return false;
     }
-    if(senha.value !== repetir_senha.value){
-        valido = false;
+    if (senha.value !== repetir_senha.value) {
         cria_erro(repetir_senha, 'O campo senha e repetir senha devem ser iguais');
+        return false;
     }
     return valido;
+}
+
+//função para validar o usuario, se ele existe ou não
+function valida_usuario() {
+    let usuarios = JSON.parse(localStorage.getItem("usuarios_cadastrados"));
+    if (usuarios) {
+        for (let i = 0; i < usuarios.length; i++) {
+            if(usuarios[i].usuario == usuario.value){
+                cria_erro(usuario, 'Usuário já cadastrado no sistema')
+                return false;
+            }   
+        }
+    }
+    return true;
 }
 
 //função para validar todos os campos, checando se eles estão vazios
-function valida_campos(){
-    let valido = true;
-    for( let campo of formulario.querySelectorAll('.validar')){
-        if(!campo.value){
+function valida_campos() {
+    for (let campo of formulario.querySelectorAll('.validar')) {
+        if (!campo.value) {
             cria_erro(campo, 'Preencha o campo');
-            valido = false;
+            return false;
         }
     }
-    return valido;
+    return true;
 }
 
-
 //adicionando evento de clique no botão olho de mostrar senha
-mostrar_senha.onclick = e =>{
-    let form_senha = document.querySelectorAll('[inputsenha]')
-    Array.from(form_senha).forEach(e =>{
-        if(e.type == 'password'){
-            e.type = 'text'
+mostrar_senha.onclick = e => {
+    let form_senha = document.querySelectorAll('[inputsenha]');
+    Array.from(form_senha).forEach(e => {
+        if (e.type == 'password') {
+            e.type = 'text';
         }
-        else{
-            e.type = 'password'
+        else {
+            e.type = 'password';
         }
     })
 }
 
-const adiciona_armazenamento_local = (user, password) =>{
+//adiciona o usuário cadastrado no localstorage
+const adiciona_armazenamento_local = (user, password) => {
     let user_dados = {
         usuario: user,
         senha: password,
     }
-    usuarios_cadastrados.push(user_dados)
-    localStorage.setItem("usuarios_cadastrados", JSON.stringify(usuarios_cadastrados))
+    usuarios_cadastrados.push(user_dados);
+    localStorage.setItem("usuarios_cadastrados", JSON.stringify(usuarios_cadastrados));
 }
--- assets/js/login.js
+++ assets/js/login.js
@@ -6,9 +6,51 @@ const botao = document.querySelector('.button_login_cadastro')
 const mostrar_senha = document.querySelector('.img_mostrar_senha')
 
 //lista local storage
-const usuarios_cadastrados = JSON.parse(localStorage.getItem("usuarios_cadastrados"))
+const usuarios = JSON.parse(localStorage.getItem("usuarios_cadastrados"))
 
-formulario.addEventListener('submit', e =>{
-    e.preventDefault()
-    console.log(usuarios_cadastrados)
+formulario.addEventListener('submit', e => {
+    handleSubmit(e)
 })
+
+function handleSubmit(e) {
+    remove_erros();
+    e.preventDefault();
+
+    const validado = valida_login();
+
+    if (validado) {
+        alert('Usuário Logado')
+        formulario.submit()
+        window.location.assign('index.html')
+
+    }
+}
+
+
+function valida_login() {
+    if(usuarios){
+        for(let i = 0; i < usuarios.length; i++){
+            if(usuarios[i].usuario == usuario.value && usuarios[i].senha == senha.value){
+                return true;
+            }
+        }
+    }
+    cria_erro(usuario, 'Usuário ou senha inválido')
+    return false;
+}
+
+//remove todas as mensagens de erro
+function remove_erros() {
+    const erros = formulario.querySelectorAll('.mensagem_erro');
+    Array.from(erros).forEach(e => {
+        e.remove();
+    })
+}
+
+//cria uma mensagem de erro
+function cria_erro(campo, msg) {
+    const erro_p = document.createElement('p');
+    erro_p.innerHTML = msg;
+    erro_p.classList.add('mensagem_erro');
+    campo.insertAdjacentElement('beforebegin', erro_p);
+}
